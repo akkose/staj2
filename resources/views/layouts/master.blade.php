804fafe6cf7e67f717bd5b3976410b6cd88f6520
AliKamilKose <alikamilkose@gmail.com>
2020-07-23 15:54:13 +0300

    ucuncu yukleme

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,16 +1,19 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="{{config('app.locale')}}">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title',config('app.name'))</title>
+    @include('layouts.partials.head')
+    @yield('head')
 </head>
-<body>
+
+<body id="commerce">
 @include('layouts.partials.navbar')
-<hr>
 @yield('content')
-<hr>
+@include('layouts.partials.footer')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+@yield('footer')
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'AnasayfaController@index')->name('anasayfa');
+Route::get('/kategori/{slug_kategoriadi}', 'KategoriController@index')->name('kategori');
+Route::get('/urun/{slug_urunadi}', 'UrunController@index')->name('urun');
+Route::get('/sepet', 'SepetController@index')->name('sepet');
 
-Route::view('/kategori','kategori');
-Route::view('/urun','urun');
-Route::view('/sepet','sepet');
+
